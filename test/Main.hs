b37bf517eaff9e1c8d7854f3cd272ad40769ca76
toyboot4e <toyboot4e@gmail.com>
2024-10-06 19:43:26 +0900

    add more test boilerplates

--- test/Main.hs
+++ test/Main.hs
@@ -3,13 +3,17 @@ module Main (main) where
 import Test.Tasty (defaultMain, testGroup)
 import Tests.DSU qualified
 import Tests.FenwickTree qualified
+import Tests.Math qualified
 import Tests.MaxFlow qualified
+import Tests.MinCostFLow qualified
 
 main :: IO ()
 main =
   defaultMain
     . testGroup "toplevel"
-    $ [ testGroup "Tests.MaxFlow" Tests.MaxFlow.tests,
-        testGroup "Tests.DSU" Tests.DSU.tests,
-        testGroup "Tests.FenwickTree" Tests.FenwickTree.tests
+    $ [ testGroup "Tests.DSU" Tests.DSU.tests,
+        testGroup "Tests.FenwickTree" Tests.FenwickTree.tests,
+        testGroup "Tests.Math" Tests.DSU.tests,
+        testGroup "Tests.MaxFlow" Tests.MaxFlow.tests,
+        testGroup "Tests.MinCostFlow" Tests.MinCostFlow.tests
       ]
