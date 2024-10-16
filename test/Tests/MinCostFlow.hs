29bf2799329f46eb0d9f5d8678a4b0b3c484f443
toyboot4e <toyboot4e@gmail.com>
2024-10-16 19:38:53 +0900

    add MCF unit tests

--- test/Tests/MinCostFlow.hs
+++ test/Tests/MinCostFlow.hs
@@ -3,8 +3,11 @@ module Tests.MinCostFlow (tests) where
 
 import AtCoder.MinCostFlow qualified as MCF
 import Data.Vector.Unboxed qualified as VU
+import System.IO.Unsafe (unsafePerformIO)
+import Test.Hspec
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.Hspec
 
 zero :: TestTree
 zero = testCase "zero" $ do
@@ -30,8 +33,60 @@ simple = testCase "simple" $ do
   ((2, 3, 1, 1, 1) @?=) =<< MCF.getEdge g 3
   ((1, 2, 1, 0, 1) @?=) =<< MCF.getEdge g 4
 
-  return ()
+usage :: TestTree
+usage = testCase "simple" $ do
+  do
+    g <- MCF.new @Int 2
+    (@?= 0) =<< MCF.addEdge g 0 1 (1 :: Int) (2 :: Int)
+    (@?= (1, 2)) =<< MCF.flow g 0 1 maxBound
+  do
+    g <- MCF.new @Int 2
+    (@?= 0) =<< MCF.addEdge g 0 1 (1 :: Int) (2 :: Int)
+    let expected = VU.fromList [(0, 0), (1, 2)]
+    (@?= expected) =<< MCF.slope g 0 1 maxBound
+
+-- assign is skipped
+
+outOfRange :: IO TestTree
+outOfRange = testSpec "outOfRange" $ do
+  g <- runIO $ MCF.new @Int @Int 10
+  it "throws error" $ do
+    MCF.slope g (-1) 3 maxBound `shouldThrow` anyException
+  it "throws error" $ do
+    MCF.slope g 3 3 maxBound `shouldThrow` anyException
+
+selfLoop :: TestTree
+selfLoop = testCase "selfLoop" $ do
+  g <- MCF.new @Int 3
+  (@?= 0) =<< MCF.addEdge g 0 0 (100 :: Int) (123 :: Int)
+  (@?= (0, 0, 100, 0, 123)) =<< MCF.getEdge g 0
+
+sameCostPaths :: TestTree
+sameCostPaths = testCase "sameCostPaths" $ do
+  g <- MCF.new @Int @Int 3
+  (0 @?=) =<< MCF.addEdge g 0 1 1 1
+  (1 @?=) =<< MCF.addEdge g 1 2 1 0
+  (2 @?=) =<< MCF.addEdge g 0 2 2 1
+  let expected = VU.fromList [(0, 0), (3, 3)]
+  (@?= expected) =<< MCF.slope g 0 2 maxBound
+
+invalid :: IO TestTree
+invalid = testSpec "invalid" $ do
+  g <- runIO $ MCF.new @Int @Int 2
+  it "throws error" $ do
+    MCF.addEdge g 0 0 (-1) 0 `shouldThrow` anyException
+  it "throws error" $ do
+    MCF.addEdge g 0 0 0 (-1) `shouldThrow` anyException
+
+-- TODO: streee test
 
 tests :: [TestTree]
-tests = [zero, simple]
-
+tests =
+  [ zero,
+    simple,
+    usage,
+    unsafePerformIO outOfRange,
+    selfLoop,
+    sameCostPaths,
+    unsafePerformIO invalid
+  ]
