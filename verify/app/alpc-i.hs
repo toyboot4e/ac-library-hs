292d2821c805696c38f2edce60b9e8989ba3ee76
toyboot4e <toyboot4e@gmail.com>
2024-11-19 21:57:21 +0900

    verify ALPC - I

--- verify/app/alpc-i.hs
+++ verify/app/alpc-i.hs
@@ -1,6 +1,16 @@
-import Util
+import AtCoder.String qualified as AS
+import Data.ByteString.Char8 qualified as BS
+import Data.Vector.Unboxed qualified as VU
+
+countUniqueSubstringsBS :: BS.ByteString -> Int
+countUniqueSubstringsBS bs = (n * (n + 1)) `div` 2 - VU.sum lcp
+  where
+    n = BS.length bs
+    sa = AS.suffixArrayBS bs
+    lcp = AS.lcpArrayBS bs sa
 
 -- verification-helper: PROBLEM https://atcoder.jp/contests/practice2/tasks/practice2_i
 main :: IO ()
 main = do
-  pure ()
+  s <- BS.getLine
+  print $ countUniqueSubstringsBS s
