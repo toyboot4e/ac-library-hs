c84d24cc4a4d4c188369983b3a97a10209504891
toyboot4e <toyboot4e@gmail.com>
2024-12-07 03:15:26 +0900

    add missing `alpc-f-mod.hs`

---
+++ verify/app/alpc-f-mod.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE DataKinds #-}
+
+import Data.ByteString.Char8 qualified as BS
+import AtCoder.Convolution qualified as C
+import AtCoder.ModInt qualified as Mint
+import Data.Vector.Unboxed qualified as VU
+import Util
+
+modInt :: Int -> Mint.ModInt998244353
+modInt = Mint.new
+
+-- verification-helper: PROBLEM https://atcoder.jp/contests/practice2/tasks/practice2_f
+main :: IO ()
+main = do
+  _ <- BS.getLine
+  a <- VU.map modInt <$> ints
+  b <- VU.map modInt <$> ints
+  let c = C.convolution a b
+  printBSB $ unwordsBSB $ VU.map fromIntegral c
