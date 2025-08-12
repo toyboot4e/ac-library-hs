53ce43fb54d2d3ffebfad06f41f0de06f2ea14f5
toyboot4e <toyboot4e@gmail.com>
2025-08-12 16:08:54 +0900

    remove `oj-verify` definition from `flake.nix`

--- flake.nix
+++ flake.nix
@@ -25,30 +25,6 @@
         ghcpkgs = import nixpkgs-for-ghc {
           inherit system;
         };
-        oj-verify =
-          with pkgs.python3Packages;
-          pkgs.python3Packages.buildPythonApplication {
-            name = "verification-helper";
-            version = "5.6.0";
-            pyproject = true;
-            src = pkgs.fetchFromGitHub {
-              owner = "online-judge-tools";
-              repo = "verification-helper";
-              rev = "adbff121b1f96de5f34e9f1483eb47d661c54075";
-              fetchSubmodules = false;
-              sha256 = "sha256-f7Ge8kLRQv9uxdNGtgNsypGVY0XAnKPCg8HYQ5nT6mI=";
-            };
-            build-system = [ setuptools ];
-            dependencies = [
-              colorlog
-              importlab
-              online-judge-tools
-              pyyaml
-              setuptools
-              toml
-            ];
-            propagatedBuildInputs = [ setuptools ];
-          };
         # TODO:
         competitive-verifier =
           with pkgs.python3Packages;
@@ -102,7 +78,7 @@
               # verify
               online-judge-tools
               competitive-verifier
-              oj-verify
+              online-judge-verify-helper
 
               # GHC 9.8.4
               (ghcpkgs.haskell.compiler.ghc984.override { useLLVM = true; })
