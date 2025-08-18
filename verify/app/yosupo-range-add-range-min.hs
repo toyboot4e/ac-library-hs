bb4b83a33fc29ad1556847e7a7f8205a16e76248
toyboot4e <toyboot4e@gmail.com>
2025-08-19 07:52:50 +0900

    verify Range Add Range Min

---
+++ verify/app/yosupo-range-add-range-min.hs
@@ -0,0 +1,28 @@
+import AtCoder.LazySegTree qualified as LST
+import AtCoder.Extra.Monoid.RangeAdd (RangeAdd (..))
+import Data.Semigroup (Min (..))
+import Data.Vector.Unboxed qualified as VU
+import Util
+
+-- verification-helper: PROBLEM https://judge.yosupo.jp/problem/range_add_range_min
+main :: IO ()
+main = do
+  (!_, !q) <- ints2
+  xs <- VU.map Min <$> ints
+  qs <- VU.replicateM q $ withLine $ do
+    intP >>= \case
+      0 -> (0 :: Int,,,) <$> intP <*> intP <*> intP
+      1 -> (1,,,-1) <$> intP <*> intP
+      _ -> error "unreachable"
+
+  seg <- LST.build xs
+  res <- (`VU.mapMaybeM` qs) $ \case
+    (0, !l, !r, !x) -> do
+      LST.applyIn seg l r $ RangeAdd (Min x)
+      pure Nothing
+    (1, !l, !r, !_) -> do
+      Min x <- LST.prod seg l r
+      pure $ Just x
+    _ -> error "unreachable"
+
+  printBSB $ unlinesBSB res
