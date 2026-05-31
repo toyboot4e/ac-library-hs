3d5b1edca7429afe528aed3c5b7d7ff8b31b0e85
toyboot4e <toyboot4e@gmail.com>
2026-05-31 17:48:58 +0900

    fix: remove `maxRight` from `SparseTable`
    Because the bisection method has nothing to do with the internals.
    Prefer the generic `maxRight` in `Extra.Bisect` in such case.

--- src/AtCoder/Extra/SparseTable.hs
+++ src/AtCoder/Extra/SparseTable.hs
@@ -23,15 +23,11 @@ module AtCoder.Extra.SparseTable
   ( SparseTable (..),
     new,
     prod,
-    maxRight,
-    minLeft,
   )
 where
 
-import AtCoder.Extra.Bisect qualified as B
 import AtCoder.Internal.Assert qualified as ACIA
 import AtCoder.Internal.Bit qualified as ACIB
-import Control.Monad.ST (runST)
 import Data.Bits (bit)
 import Data.Foldable (for_)
 import Data.Vector qualified as V
@@ -52,29 +48,29 @@ data SparseTable a = SparseTable
 new :: (Monoid a, VU.Unbox a) => VU.Vector a -> SparseTable a
 new xs
   | VU.null xs = SparseTable {nSt = 0, dataSt = V.empty}
-new xs = runST $ do
-  let nSt = VU.length xs
-  let h = ACIB.ceilingLog2 nSt + 1
-
-  vec <- V.unfoldrExactN h (VUM.splitAt nSt) <$> VUM.replicate (h * nSt) mempty
-  -- TODO: use VUM.copy
-  VU.iforM_ xs $ \i x -> do
-    VGM.write (vec VG.! 0) i x
-  VG.izipWithM_
-    ( \i row1 row2 -> do
-        let len = bit i
-        for_ [0 .. nSt - 1 - len] $ \j -> do
-          -- Each element in row1 corresponds to an interval of length 2^i at j:
-          --   vec[i][j] = vec[i][j] <> vec[i][j + 2^i]
-          x1 <- VGM.read row1 j
-          x2 <- VGM.read row1 (j + len)
-          VGM.write row2 j $! x1 <> x2
-    )
-    vec
-    (V.tail vec)
-
-  dataSt <- V.mapM VU.unsafeFreeze vec
-  pure SparseTable {..}
+new xs = SparseTable {..}
+  where
+    !nSt = VU.length xs
+    !h = ACIB.ceilingLog2 nSt + 1
+    !dataSt = V.unfoldrExactN h (VU.splitAt nSt) $ VU.create $ do
+      vecData <- VUM.replicate (h * nSt) mempty
+      let vec = V.unfoldrExactN h (VUM.splitAt nSt) vecData
+      -- TODO: use VUM.copy
+      VU.iforM_ xs $ \i x -> do
+        VGM.write (vec VG.! 0) i x
+      VG.izipWithM_
+        ( \i row1 row2 -> do
+            let len = bit i
+            for_ [0 .. nSt - 1 - len] $ \j -> do
+              -- Each element in row1 corresponds to an interval of length 2^i at j:
+              --   vec[i][j] = vec[i][j] <> vec[i][j + 2^i]
+              x1 <- VGM.read row1 j
+              x2 <- VGM.read row1 (j + len)
+              VGM.write row2 j $! x1 <> x2
+        )
+        vec
+        (V.tail vec)
+      pure vecData
 
 -- | \(O(1)\) Calculates \(\Pi_{i \in [l, r)} {m_i}\) for ideomponent monoid.
 prod :: (Monoid a, VU.Unbox a) => SparseTable a -> Int -> Int -> a
@@ -90,21 +86,3 @@ prod SparseTable {..} l r = case r - l of
     !_ = ACIA.checkInterval "AtCoder.Extra.SparseTable.prod" l r nSt
     logLen = ACIB.floorLog2 (r - l)
     len = bit logLen
-
--- | \(O(log n)\) Runs a bisection method over a monotonious sequence of ideomponent monoids from
--- left to right.
-maxRight :: (Monoid a, VU.Unbox a) => SparseTable a -> Int -> (a -> Bool) -> Int
-maxRight tbl@SparseTable {nSt} l p
-  | l == nSt = nSt
-  | otherwise = B.maxRight l nSt (\r -> p (prod tbl l r))
-  where
-    !_ = ACIA.runtimeAssert (0 <= l && l <= nSt) $ "AtCoder.Extra.SparseTable.maxRight: given invalid index `" ++ show l ++ "` over length `" ++ show nSt ++ "`"
-
--- | \(O(log n)\) Runs a bisection method over a monotonious sequence of ideomponent monoids from
--- right to left.
-minLeft :: (Monoid a, VU.Unbox a) => SparseTable a -> Int -> (a -> Bool) -> Int
-minLeft tbl@SparseTable {nSt} r p
-  | r == 0 = 0
-  | otherwise = B.minLeft 0 r (\l -> p (prod tbl l r))
-  where
-    !_ = ACIA.runtimeAssert (0 <= r && r <= nSt) $ "AtCoder.Extra.SparseTable.minLeft: given invalid index `" ++ show r ++ "` over length `" ++ show nSt ++ "`"
--- test/Tests/Extra/SparseTable.hs
+++ test/Tests/Extra/SparseTable.hs
@@ -3,18 +3,11 @@
 module Tests.Extra.SparseTable (tests) where
 
 import AtCoder.Extra.SparseTable qualified as Tbl
-import Data.Foldable (for_)
-import Data.List qualified as L
-import Data.Ord (Down (..), comparing)
 import Data.Semigroup (Max (..))
-import Data.Vector qualified as V
-import Data.Vector.Algorithms.Intro qualified as VAI
 import Data.Vector.Unboxed qualified as VU
-import Test.QuickCheck.Monadic as QCM
-import Test.Tasty
-import Test.Tasty.HUnit
+import Test.Tasty (TestTree)
 import Test.Tasty.QuickCheck as QC
-import Tests.Util
+import Tests.Util (intervalGen)
 import Prelude hiding (seq)
 
 data Init = Init
@@ -38,31 +31,7 @@ prop_randomTest Init {..} = Tbl.prod tbl l r QC.=== f l r
     tbl = Tbl.new xs
     f l r = VU.foldl' (<>) mempty . VU.take (r - l) $ VU.drop l xs
 
-prop_maxRight :: Init -> QC.Gen QC.Property
-prop_maxRight Init {..} = do
-  m <- QC.arbitrary
-  pure $ Tbl.maxRight tbl l (< m) QC.=== f l m
-  where
-    xs' = VU.modify VAI.sort xs
-    tbl = Tbl.new xs'
-    n = VU.length xs'
-    (!l, !_) = lr
-    f l m = (l +) . VU.length . VU.takeWhile (< m) $ VU.drop l xs'
-
-prop_minLeft :: Init -> QC.Gen QC.Property
-prop_minLeft Init {..} = do
-  m <- QC.arbitrary
-  pure $ Tbl.minLeft tbl r (< m) QC.=== f r m
-  where
-    xs' = VU.modify (VAI.sortBy (comparing Down)) xs
-    tbl = Tbl.new xs'
-    n = VU.length xs'
-    (!_, !r) = lr
-    f r m = (r -) . VU.length . VU.takeWhile (< m) . VU.drop (n - r) $ VU.reverse xs'
-
 tests :: [TestTree]
 tests =
-  [ QC.testProperty "random test" prop_randomTest,
-    QC.testProperty "maxRight" prop_maxRight,
-    QC.testProperty "minLeft" prop_minLeft
+  [ QC.testProperty "random test" prop_randomTest
   ]
